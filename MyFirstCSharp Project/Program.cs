73146320f0f2bcd8ce5937323d84665261f6a62e
uniquemozilla22 <49606627+uniquemozilla22@users.noreply.github.com>
2020-10-14 08:01:35 +0545

    Initial Commit
    This is my first C# application that is used in the academic course

---
+++ MyFirstCSharp Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstCSharp_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
